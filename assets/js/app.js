45650aae3a286627c329d14bb7a2afb60335ad7d
Gurdip Kalsi <gurdip.kalsi@live.com>
2025-04-03 23:59:42 +0100

    adding game mode screen

--- assets/js/app.js
+++ assets/js/app.js
@@ -179,6 +179,14 @@ function closeSettings() {
   getElement("selectionScreen").style.display = "block";
 }
 
+function selectLightningRound() {
+  // Hide the Game Mode screen
+  getElement("gameModeScreen").style.display = "none";
+
+  // Show the Character Selection screen
+  getElement("selectionScreen").style.display = "block";
+}
+
 // Event listeners
 function addEventListeners() {
   getElement("hiraganaBox").addEventListener("click", () => toggleSelection("hiraganaBox", hiraganaMap));
@@ -186,6 +194,7 @@ function addEventListeners() {
   getElement("startButton").addEventListener("click", startGame);
   getElement("romajiInput").addEventListener("keydown", handleKeyPress);
   getElement("backButton").addEventListener("click", goBack);
+  getElement("lightningRound").addEventListener("click", selectLightningRound);
 
   // Settings screen event listeners
   getElement("settingsButton").addEventListener("click", openSettings);
@@ -194,7 +203,8 @@ function addEventListeners() {
 
 // Initialize app
 window.onload = () => {
-  getElement("selectionScreen").style.display = "block";
+  getElement("gameModeScreen").style.display = "block";
+  getElement("selectionScreen").style.display = "none";
   getElement("gameScreen").style.display = "none";
   getElement("settingsScreen").style.display = "none";
 
